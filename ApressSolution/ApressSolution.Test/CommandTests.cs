27494a71aefb79c57d7c8138cb6895e97f0de17f
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-06-19 18:39:05 -0600

    add unit testing

---
+++ ApressSolution/ApressSolution.Test/CommandTests.cs
@@ -0,0 +1,58 @@
+using ApressSolution.Models;
+
+namespace ApressSolution.Test
+{
+    public class CommandTests
+    {
+        Command? _testCommand;
+        public CommandTests()
+        {
+            _testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "platform",
+                CommandLine = "CommandLine"
+            };
+        }
+
+        public void Dispose()
+        {
+            _testCommand = null;
+        }
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+
+            //Act
+            _testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", _testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Arrange
+
+            //Act
+            _testCommand.Platform = "xUnit";
+
+            //Assert
+            Assert.Equal("xUnit", _testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+
+            //Act
+            _testCommand.CommandLine = "dotnet test";
+
+            //Assert
+            Assert.Equal("dotnet test", _testCommand.CommandLine);
+        }
+    }
+}
