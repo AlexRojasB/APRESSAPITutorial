42e682ed4c2fdab9427f1ca04dcb6301ebeb2062
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-06-22 19:53:53 -0600

    delete unit tests

--- ApressSolution/ApressSolution.Test/CommandControllerTest.cs
+++ ApressSolution/ApressSolution.Test/CommandControllerTest.cs
@@ -179,7 +179,7 @@ namespace ApressSolution.Test
         }
 
         [Fact]
-        public void UpdateCommand_Returns2004NoContent_WhenValidObjectSubmitted()
+        public void UpdateCommand_Returns204NoContent_WhenValidObjectSubmitted()
         {
             //Arrange
             _mockRepo.Setup(repo => repo.GetCommandById(1)).Returns(new Command
@@ -222,6 +222,39 @@ namespace ApressSolution.Test
             //Act
             var result = controller.PartialCommandUpdate(0, new Microsoft.AspNetCore.JsonPatch.JsonPatchDocument<CommandUpdateDto> { });
 
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void DeleteCommand_Returns204NoContent_WhenValidObjectSubmitted()
+        {
+            //Arrange
+            _mockRepo.Setup(repo => repo.GetCommandById(1)).Returns(new Command
+            {
+                Id = 1,
+                HowTo = "mock",
+                Platform = "mock",
+                CommandLine = "mock"
+            });
+            var controller = new CommandsController(_mockRepo.Object, _mapper);
+
+            //Act
+            var result = controller.DeleteCommand(1);
+
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public void DeleteCommand_Returns404NotFound_WhenNonExistentResourceIDSubmitted()
+        {
+            //Arrange
+            _mockRepo.Setup(repo => repo.GetCommandById(0)).Returns(() => null);
+            var controller = new CommandsController(_mockRepo.Object, _mapper);
+
+            //Act
+            var result = controller.DeleteCommand(0);
             //Assert
             Assert.IsType<NotFoundResult>(result);
         }
